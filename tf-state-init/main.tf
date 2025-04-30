d409ba35135a19568cef4d1b2c924b6e452babe3
Raghu K <raghuk.vit@gmail.com>
2025-04-30 07:07:08 +0530

    RoboShop Project

--- tf-state-init/main.tf
+++ tf-state-init/main.tf
@@ -3,8 +3,8 @@ provider "azurerm" {
   subscription_id = "323379f3-3beb-4865-821e-0fff68e4d4ca"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "rdevopsb74tfstate"
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "rdevopsb84tfstates"
   resource_group_name      = "project-setup-1"
   location                 = "UK West"
   account_tier             = "Standard"
@@ -13,7 +13,7 @@ resource "azurerm_storage_account" "example" {
 
 resource "azurerm_storage_container" "tfstates" {
   name                  = "roboshop-state-files"
-  storage_account_id    = azurerm_storage_account.example.id
+  storage_account_id    = azurerm_storage_account.tfstate.id
   container_access_type = "private"
 }
 
