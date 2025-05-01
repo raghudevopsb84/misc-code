2791af6c16a649b98b6012a3180e79f24af69a91
Raghu K <raghuk.vit@gmail.com>
2025-05-01 07:22:41 +0530

    RoboShop Project

--- tools-setup/vm/main.tf
+++ tools-setup/vm/main.tf
@@ -53,3 +53,20 @@ resource "azurerm_virtual_machine" "vm" {
   }
 }
 
+resource "azurerm_dns_a_record" "public_dns_record" {
+  name                = "${var.name}-dev"
+  zone_name           = "rdevopsb84.online"
+  resource_group_name = var.rg_name
+  ttl                 = 3
+  records             = [azurerm_public_ip.publicip.ip_address]
+}
+
+
+resource "azurerm_dns_a_record" "private_dns_record" {
+  name                = "${var.name}-dev"
+  zone_name           = "rdevopsb84.online"
+  resource_group_name = var.rg_name
+  ttl                 = 3
+  records             = [azurerm_network_interface.privateip.private_ip_address]
+}
+
