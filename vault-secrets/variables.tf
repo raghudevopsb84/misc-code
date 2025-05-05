c77763c2432361de62c631208e23acc1d830dd6f
Raghu K <raghuk.vit@gmail.com>
2025-05-05 06:55:13 +0530

    RoboShop Project

--- vault-secrets/variables.tf
+++ vault-secrets/variables.tf
@@ -20,6 +20,18 @@ variable "values" {
         foo = "bar"
       }
     }
+
+    frontend = {
+      secret = "roboshop-dev"
+      value = {
+        catalogue_url = "http://catalogue-dev.rdevopsb84.online:8080/"
+        user_url      = "http://user-dev.rdevopsb84.online:8080/"
+        cart_url      = "http://cart-dev.rdevopsb84.online:8080/"
+        payment_url   = "http://payment-dev.rdevopsb84.online:8080/"
+        shipping_url  = "http://shipping-dev.rdevopsb84.online:8080/"
+      }
+    }
+
     ssh = {
       secret = "roboshop-infra"
       value = {
