1271c165986e21b18446945ea0d87448e638c2b6
Raghu K <raghuk.vit@gmail.com>
2025-05-05 07:13:50 +0530

    RoboShop Project

--- vault-secrets/variables.tf
+++ vault-secrets/variables.tf
@@ -16,8 +16,47 @@ variable "values" {
     cart = {
       secret = "roboshop-dev"
       value = {
-        zip = "zap",
-        foo = "bar"
+        REDIS_HOST     = "redis-dev.rdevopsb84.online"
+        CATALOGUE_HOST = "catalogue-dev.rdevopsb84.online"
+        CATALOGUE_PORT = 8080
+      }
+    }
+
+    catalogue = {
+      secret = "roboshop-dev"
+      value = {
+        MONGO     = "redis-dev.rdevopsb84.online"
+        MONGO_URL = "catalogue-dev.rdevopsb84.online"
+      }
+    }
+
+    user = {
+      secret = "roboshop-dev"
+      value = {
+        MONGO     = "true"
+        REDIS_URL = "redis://redis-dev.rdevopsb84.online:6379"
+        MONGO_URL = "mongodb://mongodb-dev.rdevopsb84.online:27017/users"
+      }
+    }
+
+    shipping = {
+      secret = "roboshop-dev"
+      value = {
+        CART_ENDPOINT = "cart-dev.rdevopsb84.online:8080"
+        DB_HOST       = "mysql-dev.rdevopsb84.online"
+      }
+    }
+
+    payment = {
+      secret = "roboshop-dev"
+      value = {
+        CART_HOST = "cart-dev.rdevopsb84.online"
+        CART_PORT = 8080
+        USER_HOST = "user-dev.rdevopsb84.online"
+        USER_PORT = 8080
+        AMQP_HOST = "rabbitmq-dev.rdevopsb84.online"
+        AMQP_USER = roboshop
+        AMQP_PASS = roboshop123
       }
     }
 
@@ -29,6 +68,22 @@ variable "values" {
         cart_url      = "http://cart-dev.rdevopsb84.online:8080/"
         payment_url   = "http://payment-dev.rdevopsb84.online:8080/"
         shipping_url  = "http://shipping-dev.rdevopsb84.online:8080/"
+      }
+    }
+
+    rabbitmq = {
+      secret = "roboshop-dev"
+      value = {
+        username = "roboshop"
+        password = "roboshop123"
+      }
+    }
+
+    mysql = {
+      secret = "roboshop-dev"
+      value = {
+        username = "root"
+        password = "RoboShop@1"
       }
     }
 
