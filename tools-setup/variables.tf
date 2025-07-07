04250c1c9a67ca5a3b26855c7fdf58b0d104b7a7
Raghu K <raghuk.vit@gmail.com>
2025-07-07 06:44:11 +0530

    RoboShop Project

--- tools-setup/variables.tf
+++ tools-setup/variables.tf
@@ -4,11 +4,11 @@ variable "tools" {
       vm_size = "Standard_DS1_v2"
       spot    = true
     }
-#     github-runner = {
-#       vm_size = "Standard_DS1_v2"
-#       spot    = false
-#       disk_size_gb = 50
-#     }
+    github-runner = {
+      vm_size = "Standard_DS1_v2"
+      spot    = false
+      disk_size_gb = 50
+    }
     elk-stack = {
       vm_size = "Standard_E2bds_v5"
       spot    = true
