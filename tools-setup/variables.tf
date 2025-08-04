cb40466f92b21b339d219c95f09bf0f3b0a000a6
Raghu K <raghuk.vit@gmail.com>
2025-08-04 06:43:07 +0530

    RoboShop Project

--- tools-setup/variables.tf
+++ tools-setup/variables.tf
@@ -9,14 +9,14 @@ variable "tools" {
       spot    = false
       disk_size_gb = 50
     }
-    elk-stack = {
-      vm_size = "Standard_E2bds_v5"
-      spot    = true
-    }
-    sonarqube = {
-      vm_size = "Standard_DS2_v2"
-      spot    = true
-    }
+#     elk-stack = {
+#       vm_size = "Standard_E2bds_v5"
+#       spot    = true
+#     }
+#     sonarqube = {
+#       vm_size = "Standard_DS2_v2"
+#       spot    = true
+#     }
     artifactory = {
       vm_size = "Standard_E2bds_v5"
       spot    = true
